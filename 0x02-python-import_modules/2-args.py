31d6cf9195c09b5d933438c9d54a6485724afd51
dagmawitheodros <dagemtedi22@gmail.com>
2022-06-09 13:47:45 -0700

    pyhton import

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    if len(argv) == 1:
-        print("{} arguments.".format(len(argv) - 1))
-    elif len(argv) == 2:
-        print("{} argument:".format(len(argv) - 1))
-        print("{}: {}".format(len(argv) - 1, argv[1]))
-    else:
-        print("{} arguments:".format(len(argv) - 1))
-        for i in range(1, len(argv)):
-            print("{}: {}".format(i, argv[i]))
+    length = len(argv)
+    print("{:d} {:s}{:s}".format(length - 1,
+                                 "argument" if length == 2 else "arguments",
+                                 "." if length == 1 else ":"))
+    for i in range(length):
+        if i > 0:
+            print("{:d}: {:s}".format(i, argv[i]))
